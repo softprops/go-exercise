c7c91a49b331ff14880e2b51059191880bbd8346
softprops <d.tangren@gmail.com>
2024-03-07 11:48:18 -0500

    pascalstriangle

--- pascalstriangle/pascalstriangle.go
+++ pascalstriangle/pascalstriangle.go
@@ -1,3 +1,11 @@
+// Pascal's Triangle
+//
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+//
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+//
+// #array #dp
+// https://leetcode.com/problems/pascals-triangle/description/
 package pascalstriangle
 
 func Solution(numRows int) [][]int {
