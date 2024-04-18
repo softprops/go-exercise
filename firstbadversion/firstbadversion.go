75c7a962d68057d30f5d4e2afb1e5a1d65cc87fb
softprops <d.tangren@gmail.com>
2024-04-18 11:40:21 -0400

    solve first bad version

--- firstbadversion/firstbadversion.go
+++ firstbadversion/firstbadversion.go
@@ -1,7 +1,16 @@
 // https://leetcode.com/problems/first-bad-version/description/
 package firstbadversion
 
-// intuition: versions are ordered
-func Solution(n, bad int) int {
-	return 0
+// intuition: versions are ordered (binary search)
+func Solution(n int, badVersion func(int) bool) int {
+	l, r := 1, n
+	for l <= r {
+		mid := l + (r-l)/2
+		if badVersion(mid) {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return l
 }
--- firstbadversion/firstbadversion_test.go
+++ firstbadversion/firstbadversion_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSolution(t *testing.T) {
 	type args struct {
 		n   int
-		bad int
+		bad func(int) bool
 	}
 	tests := []struct {
 		name string
@@ -15,23 +15,27 @@ func TestSolution(t *testing.T) {
 		{
 			name: "a",
 			args: args{
-				n:   5,
-				bad: 4,
+				n: 5,
+				bad: func(i int) bool {
+					return i < 5 && i > 3
+				},
 			},
 			want: 4,
 		},
 		{
 			name: "b",
 			args: args{
-				n:   1,
-				bad: 1,
+				n: 1,
+				bad: func(i int) bool {
+					return i > 0
+				},
 			},
 			want: 1,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Solution(tt.args.n); got != tt.want {
+			if got := Solution(tt.args.n, tt.args.bad); got != tt.want {
 				t.Errorf("Solution() = %v, want %v", got, tt.want)
 			}
 		})
